efa97ba77fcd7472fcee27558896ab6d6c443534
lllilyk <ym0402kkk@naver.com>
2023-12-04 21:58:41 +0900

    arguments 전달하기

---
+++ src/chapter_08/Arguments.jsx
@@ -0,0 +1,24 @@
+// arguments
+// : Event Handler에 전달할 데이터(파라미터:매개변수)
+
+// Class Component에서 사용하는 방식 - 지금은 거의 사용하지 않음
+// 1. arrow function 사용
+/*
+<button onClick={(event) => this.deleteItem(id, event)}>삭제하기</button>
+
+// 2. bind
+<button onClick={this.deleteItem.bind(this, id)}>삭제하기</button>
+*/
+
+// Function Component에서 사용하는 방식 
+function MyButton(props){
+    const handleDelete = (id, event) => {
+        console.log(id, event.target);
+    };
+
+    return (
+        <button onClick={(event) => handleDelete(1, event)}>
+            삭제하기
+        </button>
+    );
+}

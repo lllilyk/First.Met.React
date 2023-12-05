50f5d1bc6b6d80457056385cddbfad2722d14da5
lllilyk <ym0402kkk@naver.com>
2023-12-05 23:24:26 +0900

    JavaScript의 Truthy와 falsy

---
+++ src/chapter_09/TruthyAndFalsy.jsx
@@ -0,0 +1,22 @@
+// Truthy : true는 아니지만 true로 여겨지는 값
+/* ex)
+true
+{} (empty object)
+[] (empty array)
+42 (number, not zero)
+"0", "false" (string, not empty)
+*/
+
+// Falsy : false는 아니지만 false로 여겨지는 값
+/* ex)
+false
+0, -0 (zero, minus zero)
+0n (BigInt zero)
+'', "", `` (empty string)
+null
+undefined
+NaN (not a number)
+*/
+
+
+

d97829629a321b1cad6a65be36c477c6cf33c847
lllilyk <ym0402kkk@naver.com>
2023-12-06 23:04:30 +0900

    조건에 따른 렌더링 구현

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import LogIn from './chapter_11/LogIn';
-import MainPage from './chapter_15/MainPage';
-import ConfirmButton2 from './chapter_08/ConfirmButton2';
+import LandingPage from './chapter_09/LandingPage';
 
 const rootNode = document.getElementById('root');
 const root = ReactDOM.createRoot(rootNode);
 
   root.render(
-    <ConfirmButton2 />
+    <LandingPage />
   );
 
 // If you want to start measuring performance in your app, pass a function

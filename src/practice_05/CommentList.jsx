a6e7f835b0801f0e7b0033c8159f1b9d38bac0ae
lllilyk <ym0402kkk@naver.com>
2023-12-12 23:51:55 +0900

    댓글 데이터를 별도의 객체로 분리해서 동적으로 받아온 데이터 표시

--- src/practice_05/CommentList.jsx
+++ src/practice_05/CommentList.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import Comment from "./Comment";
 
+const comments = [
+    {
+        name: "윰니",
+        comment: "두 번째예용 이제 이해가 좀 돼용",
+    },
+    {
+        name: "근데",
+        comment: "왜 사이즈가 이상할까 촴나",
+    },
+    {
+        name: "내일",
+        comment: "확인해보기로 해..",
+    },
+];
+
 function CommentList(props) {
     return (
         <div>
-            <Comment name={"윰니"} comment={"두 번째예용 이제 이해가 좀 돼용"}/>
-            <Comment name={"왜!"} comment={"뭐가 문제얏!! 왜 사이즈가 이상하지"} />
+            {comments.map((comment) => {
+                return (
+                    <Comment name={comment.name} comment={comment.comment} />
+                )
+            })}
         </div>
     );
 }

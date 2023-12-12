a3b63c9eed8ffd3843c94a81e73db1f25865c95d
lllilyk <ym0402kkk@naver.com>
2023-12-12 23:48:31 +0900

    props를 사용하도록 Comment 컴포넌트 변경

--- src/practice_05/Comment.jsx
+++ src/practice_05/Comment.jsx
@@ -1,9 +1,64 @@
 import React from "react";
 
+//learn.node.com에 내가 css 작성한거랑 비교해보기
+const styles = {
+    wrapper: {
+        margin: 8,
+        padding: 8,
+        display: "flex",
+        flexDirection: "row",
+        border: "1px solid grey",
+        borderRadius: 16,
+    },
+    imageContainer: {},
+    image: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+    },
+    contentContainer: {
+        marginLeft: 8,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+    },
+    nameText: {
+        color: "black",
+        fontSize: 16,
+        fontWeight: "bold",
+    },
+    commentText: {
+        color: "black",
+        fontSize: 16,
+    },
+};
+
+/*
 function Comment(props) {
     return (
         <div>
             <h1>컴포넌트예용 두 번째로 듣는거예용</h1>
+        </div>
+    );
+}
+*/
+
+function Comment(props) {
+    return (
+        <div style={styles.wrapper}>
+            <div style={styles.imageContainer}>
+                <img 
+                    src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png" 
+                    alt={styles.image} 
+                />
+            </div>
+
+            <div style={styles.contentContainer}>
+                <span style={styles.nameText}>{props.name}</span>
+                <span style={styles.commentText}>
+                    {props.comment}
+                </span>
+            </div>
         </div>
     );
 }
--- src/practice_05/CommentList.jsx
+++ src/practice_05/CommentList.jsx
@@ -4,7 +4,8 @@ import Comment from "./Comment";
 function CommentList(props) {
     return (
         <div>
-            <Comment />
+            <Comment name={"윰니"} comment={"두 번째예용 이제 이해가 좀 돼용"}/>
+            <Comment name={"왜!"} comment={"뭐가 문제얏!! 왜 사이즈가 이상하지"} />
         </div>
     );
 }

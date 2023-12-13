82662d80cc344f9fa63822a8adccf7c84264d70d
lllilyk <ym0402kkk@naver.com>
2023-12-13 23:14:30 +0900

    Component Mount & Update

--- src/practice_06/Noti.jsx
+++ src/practice_06/Noti.jsx
@@ -22,6 +22,22 @@ class Noti extends React.Component {
         this.state = {};
     }
 
+    // 문장을 묶을 때 일반 따옴표가 아닌 역따옴표(`)를 사용해서 묶어줌!!!
+    // 컴포넌트가 마운트 된 이후 호출됨
+    componentDidMount() {
+        console.log(`${this.props.id} componentDidMount() called.`);
+    }
+
+    // 컴포넌트가 업데이트 된 이후 호출됨
+    componentDidUpdate() {
+        console.log(`${this.props.id} componentDidUpdate() called.`);
+    }
+
+    // 컴포넌트가 언마운트 되기 전에 호출됨
+    componentWillUnmount() {
+        console.log(`${this.props.id} componentWillUnmount() called.`);
+    }
+
     render() {
         return (
             <div style={styles.wrapper}>
--- src/practice_06/NotiList.jsx
+++ src/practice_06/NotiList.jsx
@@ -3,12 +3,15 @@ import Noti from "./Noti";
 
 const reservedNotifications = [
     {
+        id: 1,
         message: "Kissing in the moonlight",
     },
     {
+        id: 2,
         message: "Movies on the late night",
     },
     {
+        id: 3,
         message: "Getting old(it's getting old)",
     },
 ];
@@ -39,11 +42,19 @@ class NotiList extends React.Component {
         }, 1000);
     }
 
+    // notifications.props에 전달할 key와 id 추가
+    // key는 react element를 구분하기 위한 고유의 값으로 map 함수를 사용하는 경우 필수!
     render() {
         return (
             <div>
                 {this.state.notifications.map((notification) => {
-                    return <Noti message={notification.message} />
+                    return (
+                        <Noti 
+                            key={notification.id}
+                            id={notification.id}
+                            message={notification.message} 
+                        />
+                    );
                 })}
             </div>
         );

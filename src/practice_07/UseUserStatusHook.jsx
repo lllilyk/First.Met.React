3ad1c7290981f2cfae0a6140d9ab8661ed1612ff
lllilyk <ym0402kkk@naver.com>
2023-12-16 17:02:56 +0900

    왜 props.userId가 아니고 props.user.id?

---
+++ src/practice_07/UseUserStatusHook.jsx
@@ -0,0 +1,18 @@
+function UserStatus(props) {
+    const isOnline = useUserStatus(props.user.id);
+
+    if(isOnline === null) {
+        return '대기중...';
+    }
+    return isOnline ? '온라인' : '오프라인';
+}
+
+function UserListItem(props){
+    const isOnline = useUserStatus(props.user.id);
+
+    return(
+        <li style={{ color : isOnline ? 'green' : 'black' }}>
+            {props.user.name}
+        </li>
+    );
+}

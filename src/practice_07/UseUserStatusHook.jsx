1a85278535e732274bd045ffbb272d1dd450b4a4
lllilyk <ym0402kkk@naver.com>
2023-12-17 23:30:23 +0900

    두 개의 컴포넌트에서 일관성을 유지하며 하나의 hook 호출하기

--- src/practice_07/UseUserStatusHook.jsx
+++ src/practice_07/UseUserStatusHook.jsx
@@ -1,3 +1,10 @@
+// useUserStatus 훅(함수)의 선언 부분에서 function useUserStatus(userId)라고 되어있으므로,
+// userId가 이 함수(useUserStatus)의 매개변수(parameter)로 간주됨.
+// 따라서 useUserStatus 훅을 호출할 때 userId값을 전달해야 함!!
+
+// 두 컴포넌트 모두 props.userId를 사용해야 일관성이 있음
+// 이렇게 사용하면 두 컴포넌트에서 같은 인터페이스를 유지하며 useUserStatus 훅을 호출하게 됨.
+
 function UserStatus(props) {
     const isOnline = useUserStatus(props.userId);
 

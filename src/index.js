c2aad0fb2878248ec7e42c6e9e927f3bef206763
lllilyk <ym0402kkk@naver.com>
2023-12-31 23:34:17 +0900

    섭씨온도와 화씨온도 표시하기 실습

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import ShowPage from './practice_09/ShowPage';
-import AttendanceBook from './chapter_10/AttendanceBook';
+import Calculator from './chapter_12/Calculator';
 const rootNode = document.getElementById('root');
 const root = ReactDOM.createRoot(rootNode);
 
   root.render(
-    <AttendanceBook />
+    <Calculator />
   );
 
 // If you want to start measuring performance in your app, pass a function

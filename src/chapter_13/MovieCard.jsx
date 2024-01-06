0ae2263fb766009ef61452545b622940641681d6
lllilyk <ym0402kkk@naver.com>
2024-01-06 23:19:07 +0900

    시간 차이를 두고 보이게 하고 싶음

--- src/chapter_13/MovieCard.jsx
+++ src/chapter_13/MovieCard.jsx
@@ -2,10 +2,18 @@ import Card from "./Card";
 
 function MovieCard(props) {
     return (
-        <Card title="클레오의 세계" backgroundColor="#dba2ce">
-            <h3>오늘 보고 싶었던 영화</h3>
-            <h3>괴물도 한 번 더 보고 싶음..</h3>
-        </Card>
+        <div>
+            <Card title="클레오의 세계" backgroundColor="#dba2ce">
+                <h3>오늘 보고 싶었던 영화</h3>
+            </Card>
+            <Card title="사랑은 낙엽을 타고" backgroundColor="#ebcfa2">
+                <h3>메가박스에는 있지도 않어..</h3>
+            </Card>
+            <Card title="이터널 선샤인" backgroundColor="#95abe8">
+                <h3>에무시네마 함 가 말아</h3>
+                <h3>괴물도 한 번 더 보고 싶음</h3>
+            </Card>
+        </div>  
     );
 }
 
